68e7db0e114344db589524d5b7c245abe64c7c14
ekanoje <ekanoje@pdx.edu>
2014-01-22 00:32:04 -0800

    branch instruction added

---
+++ source/branch.v
@@ -0,0 +1,101 @@
+// This function handles Branch instruction
+
+function Branch_instruction;
+  input [15:0]instruction;
+  
+  
+  begin
+    Branch_instruction=0;
+    
+    case({instruction[15],instruction[10:8]})
+      
+      BR: 
+      R[PC] = R[PC]+(2*instruction[7:0]);
+      
+      BNE:
+      if(!PSW[ZERO])
+            begin
+              R[PC]= R[PC]+(instruction[7:0]*2);
+            end
+        
+        BEQ:
+        if(PSW[PSW[ZERO]])
+        begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+        end
+        
+        BPL:
+        if(!PSW[NEGATIVE])
+          begin
+            R[PC]= (R[PC]+(instruction[7:0]*2));
+          end
+          
+          BMI:
+          if(PSW[NEGATIVE])
+          begin
+            R[PC]= (R[PC]+(instruction[7:0]*2));
+            PSW[PSW[NEGATIVE]] = 0;
+          end
+          
+          BVC:
+          if(!PSW[OVERFLOW])
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BVS:
+          if(PSW[OVERFLOW])
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BCC:
+          if(!PSW[CARRY])
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+          
+          BLO:
+          if(PSW[CARRY])
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BGE:
+          if(!(PSW[NEGATIVE] || PSW[OVERFLOW]))
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BLT:
+          if(PSW[NEGATIVE] || PSW[OVERFLOW])
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BGT:
+          if(!(PSW[ZERO] || (PSW[NEGATIVE] ^ PSW[OVERFLOW])))
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BLE:
+         if(PSW[ZERO] || (PSW[NEGATIVE] ^ PSW[OVERFLOW]))
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+            
+          BHI:
+          if(!(PSW[CARRY] && PSW[ZERO]))
+             begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+             end
+            
+          BLOS:
+          if(PSW[CARRY]&&PSW[ZERO])
+            begin
+              R[PC]= (R[PC]+(instruction[7:0]*2));
+            end
+      endcase
+     end
+  endfunction    
--- source/config.v
+++ source/config.v
@@ -27,7 +27,7 @@ parameter REGISTER               = 3'b000,
           INDEX_DEFERRED         = 3'b111;
                 
                 
-//Parameters of Double Operand Instructions 
+//Parameters of Double Operand Instructions & one and half operand instruction 
 parameter MOV  = 4'b0001,
           MOVB = 4'b1001,
           CMP  = 4'b0010,
@@ -39,24 +39,32 @@ parameter MOV  = 4'b0001,
           BIS  = 4'b0101,
           BISB = 4'b1101,
           ADD  = 4'b0110,
-          SUB  = 4'b1110;   
+          SUB  = 4'b1110;
+          
+// Parameters for branch instruction 
 
-//Parameters of Single Operand Instructions
-parameter SWAB = 10'b0000000000,
-					CLR  = 5'b01000,
-					COM  = 5'b01001,
-					INC  = 5'b01010,
-					DEC  = 5'b01011,
-					NEG  = 5'b01100,
-					ADC  = 5'b01101,
-					SBC  = 5'b01110,
-					TST  = 5'b01111,
-          ROR	 = 5'b10000,
-					ROL	 = 5'b10001,
-				  ASR  = 5'b10010,
-					ASL  = 5'b10011,
-					MARK = 5'b10100,		// byte variant of this has different functionality, take note
-					MFPI = 5'b10101,
-					MTPI = 5'b10110,
-					SXT	 = 5'b10111;
-					
+parameter BR  = 4'b0001,
+          BNE = 4'b0010,
+          BEQ = 4'b0011,
+          BPL = 4'b1000,
+          BMI = 4'b1001,
+          BVC = 4'b1100,
+          BVS = 4'b1101,
+          BCC= 4'b1110,
+          BLO = 4'b1111,
+          BGE = 4'b0100,
+          BLT = 4'b0101,
+          BGT = 4'b0110,
+          BLE = 4'b0111,
+          BHI = 4'b1010,
+          BLOS= 4'b1011;
+          
+
+// Parameters for Jump instruction
+          
+          
+
+          
+                
+         
+                        
